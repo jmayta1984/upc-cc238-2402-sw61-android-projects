640cecebb95427c74923bd6a1ce46d7803d69d8f
jmayta1984 <jmayta@pucp.edu.pe>
2024-09-19 14:03:50 -0500

    Update JokeRepository. Add CRUD operations.

--- JokesCompose/app/src/main/java/pe/edu/upc/jokescompose/data/repository/JokeRepository.kt
+++ JokesCompose/app/src/main/java/pe/edu/upc/jokescompose/data/repository/JokeRepository.kt
@@ -3,18 +3,41 @@ package pe.edu.upc.jokescompose.data.repository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import pe.edu.upc.jokescompose.common.Resource
+import pe.edu.upc.jokescompose.data.local.JokeDao
+import pe.edu.upc.jokescompose.data.local.JokeEntity
+import pe.edu.upc.jokescompose.data.remote.JokeDto
 import pe.edu.upc.jokescompose.data.remote.JokeService
 import pe.edu.upc.jokescompose.data.remote.toJoke
 import pe.edu.upc.jokescompose.domain.Joke
 
-class JokeRepository(private val service: JokeService) {
+class JokeRepository(
+    private val service: JokeService,
+    private val dao: JokeDao
+) {
+
+    suspend fun insertJoke(joke: Joke) = withContext(Dispatchers.IO) {
+        dao.insert(JokeEntity(joke.description, joke.score))
+    }
+
+    suspend fun deleteJoke(joke: Joke) = withContext(Dispatchers.IO) {
+        dao.delete(JokeEntity(joke.description, joke.score))
+    }
+
+    suspend fun updateJoke(joke: Joke) = withContext(Dispatchers.IO) {
+        dao.update(JokeEntity(joke.description, joke.score))
+    }
 
     suspend fun getJoke(): Resource<Joke> = withContext(Dispatchers.IO) {
         val response = service.getJoke()
 
         if (response.isSuccessful) {
             response.body()?.let { jokeDto ->
-                return@withContext Resource.Success(data = jokeDto.toJoke())
+                val joke = jokeDto.toJoke()
+                dao.fetchByDescription(joke.description)?.let { jokeEntity ->
+                    joke.score = jokeEntity.score
+                }
+
+                return@withContext Resource.Success(data = joke)
             }
             return@withContext Resource.Error(message = response.message())
         }
