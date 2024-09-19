529f9e66ae844e4fa7d61a70e285f13aa6de04be
jmayta1984 <jmayta@pucp.edu.pe>
2024-09-19 15:01:02 -0500

    Update project structure

--- JokesCompose/app/src/main/java/pe/edu/upc/jokescompose/core/AppDatabase.kt
+++ JokesCompose/app/src/main/java/pe/edu/upc/jokescompose/core/AppDatabase.kt
@@ -1,7 +1,9 @@
-package pe.edu.upc.jokescompose.data.local
+package pe.edu.upc.jokescompose.core
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import pe.edu.upc.jokescompose.data.local.JokeDao
+import pe.edu.upc.jokescompose.data.local.JokeEntity
 
 @Database(entities = [JokeEntity::class], version = 1)
 abstract class AppDatabase: RoomDatabase() {
